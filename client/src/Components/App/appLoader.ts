33c8e2126bb27f23b5eef6486ffce9d1d3cb4a08
mjarvis1997 <mjarvis1997@gmail.com>
2023-01-09 01:57:27 -0500

    cleanup appLoader

--- client/src/Components/App/appLoader.ts
+++ client/src/Components/App/appLoader.ts
@@ -3,7 +3,6 @@
 import { useAppDispatch } from 'Hooks/hooks';
 import { AppDispatch } from 'Hooks/store';
 import { authenticate } from 'Services';
-import { User } from 'Types';
 import { APP_ACTIONS } from './appSlice';
 
 export class AppLoader {
@@ -13,33 +12,18 @@ export class AppLoader {
     this.dispatch = useAppDispatch();
   }
 
-  private userPending = () => this.dispatch(APP_ACTIONS.setIsUserLoading(true));
-
-  // Helper functions for user status
-  private userSuccess = (user: User) => {
-    this.dispatch(APP_ACTIONS.setUser(user));
-    this.dispatch(APP_ACTIONS.setIsUserLoading(false));
-  };
-
-  private userFailure = (message: string) => {
-    this.dispatch(APP_ACTIONS.setUserError(message));
-    this.dispatch(APP_ACTIONS.setIsUserLoading(false));
-  };
-
   // Attempt to load user information from accessToken
   // If successful, store in redux state
   public async loadUser() {
-    this.userPending();
-
-    // TODO: actual auth logic
+    this.dispatch(APP_ACTIONS.setIsUserLoading(true));
     const user = await authenticate();
 
     if (!user) {
-      this.userFailure('Failed to fetch user');
+      this.dispatch(APP_ACTIONS.setUserError('Failed to fetch user'));
       return false;
     }
 
-    this.userSuccess({ ...user });
+    this.dispatch(APP_ACTIONS.setUser({ ...user }));
     return true;
   }
 }
